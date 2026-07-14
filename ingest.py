53510f3cfb7a367abc0f24d296cdc7826e353372
Jona T. Feucht <14951074+jonafeucht@users.noreply.github.com>
2026-07-14 08:08:42 +0200

    +++

--- ingest.py
+++ ingest.py
@@ -11,7 +11,7 @@ DATA_DIR = Path(os.getenv("PDF_DIR", "./mcp-data")).resolve()
 OUTPUT_DIR = Path(os.getenv("DB_URI", "./mcp-server/lancedb_index")).resolve()
 
 TABLE_NAME = "document_chunks"
-EMBED_MODEL = "all-MiniLM-L6-v2"
+EMBED_MODEL = "jina-embeddings-v5-text-nano"
 
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
